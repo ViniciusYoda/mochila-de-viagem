20c3e81ccbad553aa9c8e41d6a706579b51d07a8
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-30 12:02:19 -0300

    autualizando o localStorage

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const form = docume.getElementById("novoItem")
 const lista = document.getElementById("lista")
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-itens.forEach( (elemento) => {
+itens.forEach((elemento) => {
    criaElemento(elemento)
 })
 
@@ -12,38 +12,45 @@ form.addEventListener("submit", (evento) => {
    const nome = evento.target.elements['nome']
    const quantidade = evento.target.elements['quantidade']
 
-   
+   const existe = itens.find(elemento => elemento.nome === nome.value)
 
    const itemAtual = {
-      "nome" : nome.value,
-      "quantidade" : quantidade.value
+      "nome": nome.value,
+      "quantidade": quantidade.value
    }
 
-   criaElemento(itemAtual)
+   if (existe) {
+      itemAtual.id = existe.id
 
-   itens.push(itemAtual)
+      atualizaElemento(item)
+
+      itens[existe.id] = itemAtual
+   } else {
+      itemAtual.id = itens.length
+
+      criaElemento(itemAtual)
+
+      itens.push(itemAtual)
+   }
 
    localStorage.setItem("item", JSON.stringify(itens))
 
    nome.value = ""
    quantidade.value = ""
 
-   
-
 })
 
 function criaElemento(item) {
    const novoItem = document.createElement("i")
    novoItem.classList.add("item")
-
    const numeroItem = document.createElement("strong")
-   numeroItem.innerHTML = item.quantidade   
+   numeroItem.innerHTML = item.quantidade
+   numeroItem.dataset.id = item.id
    numeroItem.appendChild(numeroItem)
-
    numeroItem.innerHTML += item.nome
-
-   
    lista.appendChild(novoItem)
+}
 
-   
+function atualizaElemento(item) {
+   document.querySelector("[data-id=" + item.id + "]").innerHTML = item.quantidade
 }
